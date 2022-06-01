9b71a0dbdfb9c891352cd1fd432fee607a760156
Muhammad Sufi Afifi <102392777+5uf@users.noreply.github.com>
2022-06-01 19:40:39 +0800

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,7 @@
+@echo off
+color a
+mode 1000
+pause
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
